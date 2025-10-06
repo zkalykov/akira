56c1fbd485885d1f4580f31c9a3b99e76ee5805d
zkalykov <zhyrgalbekkalykov@gmail.com>
2025-10-06 09:45:27 -0500

    Adding Front end

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-black px-[20%] text-center">
+      <div>
+        <h1 className="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-b from-gray-100 to-gray-400 leading-tight drop-shadow-[0_0_10px_rgba(255,255,255,0.2)]">
+          Building Akira
+        </h1>
+        <p className="mt-6 text-2xl font-semibold text-transparent bg-clip-text bg-gradient-to-b from-gray-200 to-gray-500 drop-shadow-[0_0_8px_rgba(255,255,255,0.3)]">
+          Create website in website
+        </p>
+        <p className="mt-6 text-2xl font-semibold text-transparent bg-clip-text bg-gradient-to-b from-gray-200 to-gray-500 drop-shadow-[0_0_8px_rgba(255,255,255,0.3)]">
+          Developer Team: Bekbolsun & Zhyrgalbek
+        </p>
+      </div>
+    </div>
+  );
+}
